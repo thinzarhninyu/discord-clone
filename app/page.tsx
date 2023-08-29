b50666c113b8e739d60b30c231d57603e4e45b9b
thinzarhninyu <thinzarhninyu.17@gmail.com>
2023-08-29 13:38:24 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className=''>
+      <p className='text-3xl font-bold text-indigo-500'>Hello Discord Clone</p>
+    </div>
+  )
+}
