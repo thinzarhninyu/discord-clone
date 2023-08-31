87b6d6897735c22eb4e4341e34ea6911f01a58dd
thinzarhninyu <thinzarhninyu.17@gmail.com>
2023-08-31 11:37:34 +0800

    create channel + delete server + leave server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,33 +1,58 @@
-import { currentProfile } from "@/lib/current-profile";         
+import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
-    req: Request,
-    { params }: { params: { serverId: string } }
-  ) {
-    try {
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
     const { name, imageUrl } = await req.json();
     const profile = await currentProfile();
 
-    if (!profile){
-        return new NextResponse("Unauthorized", {status: 401})
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 })
     }
 
     const server = await db.server.update({
-        where: {
-            id: params.serverId,
-            profileId: profile.id
-        },
-        data: {
-            name,
-            imageUrl
-        }
+      where: {
+        id: params.serverId,
+        profileId: profile.id
+      },
+      data: {
+        name,
+        imageUrl
+      }
     });
 
     return NextResponse.json(server);
-    } catch (error) {
-      console.log("[SERVER_ID_PATCH]", error);
-      return new NextResponse("Internal Error", { status: 500 });
-    }
+  } catch (error) {
+    console.log("[SERVER_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 })
+    }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,9 @@ import { useState, useEffect } from "react";
 import { InviteModal } from "../modals/invite-modal";
 import { EditSeverModal } from "../modals/edit-server-modal";
 import { MembersModal } from "../modals/members-modal";
+import { CreateChannelModal } from "../modals/create-channel-modal";
+import { LeaveServerModal } from "../modals/leave-server-modal";
+import { DeleteServerModal } from "../modals/delete-server-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -22,6 +25,9 @@ export const ModalProvider = () => {
             <InviteModal />
             <EditSeverModal />
             <MembersModal />
+            <CreateChannelModal />
+            <LeaveServerModal />
+            <DeleteServerModal />
         </>
     )
 }
