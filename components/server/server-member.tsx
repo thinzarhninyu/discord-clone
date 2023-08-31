1d960e953c4f91148f30bbb599bdfde9f23084ca
thinzarhninyu <thinzarhninyu.17@gmail.com>
2023-08-31 20:20:05 +0800

    channel and chat navigation

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -27,8 +27,12 @@ export const ServerMember = ({
 
     const icon = roleIconMap[member.role];
 
+    const onClick = () => {
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+    }
+
     return (
-        <button onClick={() => { }} className={cn(
+        <button onClick={onClick} className={cn(
             "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
             params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
         )}>
@@ -36,7 +40,7 @@ export const ServerMember = ({
             {icon}
             <p className={cn(
                 "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-500 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-                params?.channelId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+                params?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
             )}>{member.profile.name}</p>
         </button>
     )
